63730685191320dfaf7c93cc303ae0ff6c8c9d45
ahsan-emon <ahsan.emon23@gmail.com>
2022-06-16 00:44:53 +0600

    Add full project

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+var wpAppClose = document.getElementById('wpClose');
+var wpAppSec = document.getElementById('wa_img');
+
+wpAppClose.addEventListener("click",function(){
+    console.log("OK")
+    document.getElementById('wa_img').style.display = "none"
+});
